64b164011ef3ef78815c9a398ead883199b77e9d
Olajide olanrewaju <olajide.m.olanrewaju@gmail.com>
2022-06-13 22:35:24 +0100

    started the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react/cjs/react.development";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        enter your budget for the{" "}
+        <select name="" id="">
+          <option value="*javascript month number">month</option>
+          <option value="7">week</option>
+        </select>
+      </>
+    );
+  }
+}
