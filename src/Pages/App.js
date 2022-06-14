0e6b6aa7b024bfc9ad2eb09519089a1a39e0dc08
Olajide olanrewaju <olajide.m.olanrewaju@gmail.com>
2022-06-14 19:21:02 +0100

    just little changes

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,11 +1,19 @@
-import { Component } from "react/cjs/react.development";
+import { Component } from "react";
 
 export default class App extends Component {
   render() {
+   let textMonth = ()=>{
+     //Get the date
+let date = new Date();
+//Get hours from date
+return date.getMonth();
+
+//select the button from the DOM
+    }
     return (
       <>
         enter your budget for the{" "}
-        <select name="" id="">
+        <select className="select" id="">
           <option value="*javascript month number">month</option>
           <option value="7">week</option>
         </select>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./Pages/App";
+import "./assets/style/bootstrap.min.css"
+import "./assets/style/main.css"
 
 ReactDOM.render(<App />, document.getElementById("root"));
